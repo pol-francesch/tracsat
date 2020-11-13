2c8ce33b8364a1c1f1aa83e8913d588c27b9dc94
Pol Francesch <pfrances@purdue.edu>
2020-11-13 11:56:16 -0500

    Created solenoid test file

---
+++ src/hpl/solenoid_test.py
@@ -0,0 +1,31 @@
+# Imports
+try:
+    from pynq.overlays.base import BaseOverlay
+    from pynq.lib.arduino import Arduino_IO
+    from pynq.lib import PynqMicroblaze
+    from time import sleepprint("Excellent. Successfully imported all dependencies.")
+except ImportError as identifier:
+    print("TracSat stopped. Failed to import a required dependency. See the error below: ")
+    print(identifier)
+    exit(1)
+
+# Set up solenoids
+sol1 = Arduino_IO(base.iop_arduino.mb_info, 0, 'out')
+sol2 = Arduino_IO(base.iop_arduino.mb_info, 1, 'out')
+
+print("Testing")
+
+while True:
+    print()
+    print("Turning on solenoids")
+
+    sol1.write(0x01)
+    sol2.write(0x01)
+
+    sleep(1.0)
+    print("Turning off solenoids")
+
+    sol1.write(0x00)
+    sol2.write(0x00)
+
+    
