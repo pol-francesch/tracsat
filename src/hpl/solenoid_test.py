7e374f3c0b54e67ef4c86f4746052416c25ffb87
Pol Francesch Huc <pfrances@purdue.edu>
2020-11-13 14:13:14 -0500

    Added two more solenoids

--- src/hpl/solenoid_test.py
+++ src/hpl/solenoid_test.py
@@ -14,6 +14,8 @@ base = BaseOverlay("base.bit")
 # Set up solenoids
 sol1 = Arduino_IO(base.iop_arduino.mb_info, 0, 'out')
 sol2 = Arduino_IO(base.iop_arduino.mb_info, 1, 'out')
+sol3 = Arduino_IO(base.iop_arduino.mb_info, 2, 'out')
+sol4 = Arduino_IO(base.iop_arduino.mb_info, 3, 'out')
 
 print("Testing")
 
@@ -23,11 +25,15 @@ while True:
 
     sol1.write(0x01)
     sol2.write(0x01)
+    sol2.write(0x01)
+    sol3.write(0x01)
 
     sleep(1.0)
     print("Turning off solenoids")
 
     sol1.write(0x00)
     sol2.write(0x00)
+    sol2.write(0x00)
+    sol3.write(0x00)
 
     sleep(5)
