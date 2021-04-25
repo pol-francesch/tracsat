f522fd111420391fcfc0833fdb0469bbcd1cbe67
Pol Francesch <pfrances@purdue.edu>
2021-04-25 17:16:37 -0400

    hopefully streamlined testing procedures

--- src/oss/testbed_test/test.py
+++ src/oss/testbed_test/test.py
@@ -3,7 +3,7 @@
 # We want to save their data to files
 # Then once the test is over, we will read the files in a separate Python script and do some cool stuff with it
 
-# from lidar import Lidar
+from lidar import Lidar
 from video import Video
 import time
 from tqdm import tqdm
@@ -14,24 +14,34 @@ from tqdm import tqdm
 # LIDAR: for the object determination
 
 # Initialize components
-# lidar = Lidar()
+lidar = Lidar()
 video = Video()
 
 # Initialize settings
-compression = True
-color = "4bit"
+compression = False
+color = "8bit"
 dim = (640,480)
+addtl = "_no_8bit"
 
 video.setColor(color)
 video.setDim(dim)
 
 # Initialize file writing
-# /home/polfr/Documents/PurdueTracSat/tracsat/src/oss/testbed_test/data/video_out.txt
+source = input('Type 0 for PYNQ. Type 1 for Pol\'s PC: ')
 path_xilinx = "/home/xilinx/tracsat/src/oss/testbed_test/data/"
 path_pc = "/home/polfr/Documents/tracsat/src/oss/testbed_test/data/"
-video_file = open(path_pc + "video_out_4_yes.txt", "w")
-# raw_file = open(path_xilinx + "lidar_raw_data.txt", "w")
-# obj_file = open(path_xilinx + "obj_data.txt", "w")
+
+if source == '0':
+    path = path_xilinx
+elif source == '1':
+    path = path_pc
+else:
+    print('That is not an option!')
+    exit(0)
+
+video_file = open(path + "video_out" + addtl + ".txt", "w")
+raw_file = open(path + "lidar_raw" + addtl + ".txt", "w")
+obj_file = open(path + "obj_data" + addtl + ".txt", "w")
 
 _=input("write")
 
@@ -53,16 +63,16 @@ while time.time() < t_end:
         else:
             frame = video.getFrameBitsFast()
 
-        # # Write video frame to file
+        # Write video frame to file
         frames.append(frame)
 
-        # # Get LIDAR data
-        # raw_data = lidar.get_scan()
-        # obj_data = lidar.get_obs_data()
+        # Get LIDAR data
+        raw_data = lidar.get_scan()
+        obj_data = lidar.get_obs_data()
 
-        # # Write LIDAR data
-        # raws.append(raw_data)
-        # objs.append(obj_data)
+        # Write LIDAR data
+        raws.append(raw_data)
+        objs.append(obj_data)
 
     except KeyboardInterrupt:
         break
@@ -80,17 +90,17 @@ for i in tqdm(range(0,len(frames))):
     # Write to file
     video_file.write(frame_string + "\n")
 
-# print("File writing - lidar")
-# for raw in raws:
-#     raw_string = ",".join([str(i) for i in raw])
-#     raw_file.write(raw_string + "\n")
+print("File writing - lidar")
+for raw in raws:
+    raw_string = ",".join([str(i) for i in raw])
+    raw_file.write(raw_string + "\n")
 
-# for obj in objs:
-#     obj_string = ",".join([str(i) for i in obj])
-#     obj_file.write(obj_string + "\n")
+for obj in objs:
+    obj_string = ",".join([str(i) for i in obj])
+    obj_file.write(obj_string + "\n")
 
 # Close files
 print("Closing all files")
 video_file.close()
-# raw_file.close()
-# obj_file.close()
+raw_file.close()
+obj_file.close()
