6f34d26ba129bba36d3894b2bef7e3c2a955863f
jack440 <jiannamorelli@gmail.com>
2021-10-04 18:41:11 -0400

    Fixed code with right-handed orientation

--- src/controller/OSS.py
+++ src/controller/OSS.py
@@ -18,19 +18,11 @@ negY = 9
 posZ = 5
 negZ = 6
 
-GPIO.setup(posX, GPIO.OUT)
-GPIO.setup(negX, GPIO.OUT)
-GPIO.setup(posY, GPIO.OUT)
-GPIO.setup(negY, GPIO.OUT)
-GPIO.setup(posZ, GPIO.OUT)
-GPIO.setup(negZ, GPIO.OUT)
+directionPins = np.array([posX,negX,posY,negY,posZ,negZ])
 
-GPIO.output(posX, GPIO.LOW)
-GPIO.output(negX, GPIO.LOW)
-GPIO.output(posY, GPIO.LOW)
-GPIO.output(negY, GPIO.LOW)
-GPIO.output(posZ, GPIO.LOW)
-GPIO.output(negZ, GPIO.LOW)
+for j in range(0,6):
+    GPIO.setup(directionPins[j], GPIO.OUT)
+    GPIO.output(directionPins[j], GPIO.LOW)
 
 fileName = "log_" + str(round(time.time())) + ".txt"
 
@@ -57,7 +49,7 @@ phi = math.atan2((yW[endWaypoint]-yW[startWaypoint]),(xW[endWaypoint]-xW[startWa
 if phi < 0:
     phi += 2*math.pi
 
-waypointDist = math.sqrt(((yW[endWaypoint]-yW[startWaypoint])^2+(xW[endWaypoint]-xW[startWaypoint])^2)) #distance between waypoints
+waypointDist = math.sqrt(((yW[endWaypoint]-yW[startWaypoint])**2+(xW[endWaypoint]-xW[startWaypoint])**2)) #distance between waypoints
 
 t0 = time.time()
 
@@ -67,22 +59,27 @@ euler1, euler2, euler3 = sensor.euler
 t1 = time.time()
 
 euler1 = euler1 * math.pi/180
+euler1 = 2 * math.pi - euler1
 
 xPos, yPos, xVel, yVel = controller.updateKinematics(0,0,0,0,euler1,accX,accY,t0,t1)
-
+temp = t0
 t0 = t1
 
-errorS = waypointDist - (math.cos(phi)*(xPos - xW[startWaypoint]) + math.cos(phi)*(yPos - yW[startWaypoint])) #error along path
+errorS = waypointDist - (math.cos(phi)*(xPos - xW[startWaypoint]) + math.sin(phi)*(yPos - yW[startWaypoint])) #error along path
 errorR = -math.sin(phi)*(xPos - xW[startWaypoint]) + math.cos(phi)*(yPos - yW[startWaypoint]) #error perpendicular to path
 rT = math.atan2((objectY - yPos),(objectX - xPos)) #rotation setpoint
 if rT < 0:
     rT += math.pi*2
+
+rT = 90*math.pi/180 #hardcoding orientation to see if it works
+
 errorT = rT - euler1 #rotation error
 
 thrusters = np.zeros(6)
 
-f.write("%t0, xPos, yPos, xVel, yVel, accX, accY, euler1, errorS, errorR, errorT, thruster1, thruster2, thruster3, thruster4, thruster5, thruster6, uS, uR, uT\n")
-f.write("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(t0,xPos,yPos,xVel,yVel,accX,accY,euler1,errorS,errorR,errorT,thruster[0],thruster[1],thruster[2],thruster[3],thruster[4],thruster[5],0,0,0))
+f.write("%t0, t1, xPos, yPos, xVel, yVel, accX, accY, euler1, errorS, errorR, errorT, thruster1, thruster2, thruster3, thruster4, thruster5, thruster6, uS, uR, uT\n")
+f.write("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(temp,t0,xPos,yPos,xVel,yVel,accX,accY,euler1,errorS,errorR,errorT,thruster[0],thruster[1],thruster[2],thruster[3],thruster[4],thruster[5],0,0,0))
+f.close()
 
 objectPos = np.array([objectX, objectY])
 satPos = np.array([xPos,yPos])
@@ -97,31 +94,12 @@ prevErrors = np.array([errorS,errorR,errorT])
 waypointEdges = np.array([startWaypoint,endWaypoint])
 
 while True:
-    thrusters,satPos,satVel,integrals,prevErrors,waypointEdges,t0 = controller.pid(objectPos,satPos,satVel,integrals,gains,prevErrors,wX,wY,waypointEdges,t0,sensor,f)
+    thrusters,satPos,satVel,integrals,prevErrors,waypointEdges,t0 = controller.pid(objectPos,satPos,satVel,integrals,gains,prevErrors,xW,yW,waypointEdges,t0,sensor,fileName)
 
-    if thrusters[0] == 1:
-        GPIO.output(posX, GPIO.HIGH)
-    else:
-        GPIO.output(posX, GPIO.LOW)
-    if thrusters[1] == 1:
-        GPIO.output(negX, GPIO.HIGH)
-    else:
-        GPIO.output(negX, GPIO.LOW)
-    if thrusters[2] == 1:
-        GPIO.output(posY, GPIO.HIGH)
-    else:
-        GPIO.output(posY, GPIO.LOW)
-    if thrusters[3] == 1:
-        GPIO.output(negY, GPIO.HIGH)
-    else:
-        GPIO.output(negY, GPIO.LOW)
-    if thrusters[4] == 1:
-        GPIO.output(posZ, GPIO.HIGH)
-    else:
-        GPIO.output(posZ, GPIO.LOW)
-    if thrusters[5] == 1:
-        GPIO.output(negZ, GPIO.HIGH)
-    else:
-        GPIO.output(negZ, GPIO.LOW)
+    for j in range(0,6):
+        if thrusters[j] == 1:
+            GPIO.output(directionPins[j], GPIO.HIGH)
+        else:
+            GPIO.output(directionPins[j], GPIO.LOW)
 
     time.sleep(.015) #wait for solenoids to fully open/close
